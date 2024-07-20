2ed7c9ce5565597613b9d27ea524e93106faaf76
jianghua1 <719584530@qq.com>
2024-07-21 03:06:24 +0800

    开发消息通知组件

---
+++ src/components/Notice/Notice.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <el-dropdown trigger="click">
+      <span>
+        <Notification v-bind="$props"></Notification>
+      </span>
+      <template #dropdown>
+        <Lists :lists="lists" :actions="actions" :wrap-class="wrapClass" :wrap-style="wrapStyle" v-bind="$attrs">
+        </Lists>
+      </template>
+    </el-dropdown>
+
+  </div>
+</template>
+
+<script setup lang='ts'>
+import Lists from './NoticeMessageList.vue'
+import Notification from './Notification.vue'
+import { NoticeProps } from './types'
+
+const props = defineProps<NoticeProps>()
+
+const filterdProps = computed(() => {
+  const { actions, lists, ...restProps } = props
+  return [restProps]
+})
+
+</script>
+<style scoped></style>
--- src/components/Notice/Notification.vue
+++ src/components/Notice/Notification.vue
@@ -1,6 +1,6 @@
 <template>
   <el-badge :value="value" :max="10000" :style="{
-    '--tomic-notice-bg-color': color || 'var(--el-color-danger)',
+    // '--tomic-notice-bg-color': color || 'var(--el-color-danger)',
     '--tomic-notice-font-size': size + 'px'
   }">
     <slot>
@@ -12,31 +12,22 @@
 </template>
 
 <script setup lang='ts'>
-import { Icon, type IconifyIcon } from '@iconify/vue'
-import { BadgeProps } from 'element-plus'
-//若我们的泛型上的值不是必须传的可以添加Partial
-interface Notification extends Partial<BadgeProps> {
-  icon: string | IconifyIcon
-  size: number
-  color: string
-  // 之所以是可选的是因为默认可以是黑白的
-  iconSize?: number
-  iconColor?: string
-}
+import type { NotificationProps } from './types.d'
+import { Icon } from '@iconify/vue'
 //初始化组件参数
-withDefaults(defineProps<Notification>(), {
+const props = withDefaults(defineProps<NotificationProps>(), {
   icon: 'ep:bell',
   size: 18,
   color: ''
 })
+const colorBg = computed(() => props.color || 'var(--el-color-danger)')
 </script>
 <style lang="scss" scoped>
-$color: var(--tomic-notice-bg-color);
 $size: var(--tomic-notice-font-size);
 
 //深度选择器
 :deep(.el-badge__content) {
-  background-color: $color;
+  background-color: v-bind(colorBg);
   font-size: $size;
 }
 </style>

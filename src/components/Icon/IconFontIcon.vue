8d363c9d8ffe51137d4b2dc8bf1d613ba1740053
江华 <719584530@qq.com>
2024-07-23 23:59:02 +0800

    笔记本电脑的提交代码测试

--- src/components/Icon/IconFontIcon.vue
+++ src/components/Icon/IconFontIcon.vue
@@ -1,5 +1,5 @@
 <template>
-  <NetIcon url="//at.alicdn.com/t/c/font_4421207_cksjqt2r42n.css" v-bind="$attrs"></NetIcon>
+  <NetIcon url="//at.alicdn.com/t/c/font_4421207_cksjqt2r42n.css" v-bind="$attrs"></NetIcon> 
 </template>
 
 <script setup lang='ts'>

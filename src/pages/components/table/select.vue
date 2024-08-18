4b2bf43e365f34d01d85bfd664b0fc6a4de1babd
jianghua1 <719584530@qq.com>
2024-08-18 22:21:16 +0800

    4.3.2.6 表格示例：树形数据与合计行

--- src/pages/components/table/select.vue
+++ src/pages/components/table/select.vue
@@ -40,6 +40,20 @@
           </template>
         </VTable>
       </el-tab-pane>
+      <el-tab-pane label="树形结构" name="8">
+        <VTable :columns="treeTableColumns" :data="treeTableData" default-expand-all row-key="id" border></VTable>
+      </el-tab-pane>
+      <el-tab-pane label="表尾合计" name="9">
+        <VTable :columns="sumTableColumns" :data="sumTableData" show-summary border></VTable>
+        <VTable :columns="sumTableColumns" :data="sumTableData" show-summary :summary-method="getSummaries" border>
+        </VTable>
+      </el-tab-pane>
+      <el-tab-pane label="行和列合并" name="10">
+        <VTable :columns="sumTableColumns" :data="sumTableData" :span-method="arraySpanMethod" border></VTable>
+        <VTable :columns="sumTableColumns" :data="sumTableData" :span-method="objectSpanMethod" border
+          style="width: 100%; margin-top: 20px">
+        </VTable>
+      </el-tab-pane>
     </el-tabs>
   </div>
 </template>
@@ -138,10 +152,12 @@ const fixedTableData = [
 ]
 //多选
 interface User {
+  id?: number
   date: string
   name: string
   address: string
   tag?: string
+  children?: User[]
 }
 
 const tableData: User[] = [
@@ -346,6 +362,167 @@ const handleEdit = (index: number, row: User) => {
 const handleDelete = (index: number, row: User) => {
   console.log(index, row)
 }
+// 树形菜单
+const treeTableData: User[] = [
+  {
+    id: 1,
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles'
+  },
+  {
+    id: 2,
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+    children: [
+      {
+        id: 21,
+        date: '2016-05-02',
+        name: 'Tom Jr.',
+        address: 'No. 189, Grove St, Los Angeles'
+      }
+    ]
+  },
+  {
+    id: 3,
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles'
+  }
+]
+const treeTableColumns = [
+  { label: '日期', prop: 'date' },
+  { label: '姓名', prop: 'name' },
+  { label: '地址', prop: 'address' }
+] as TableColumnType[]
 
+//表尾合计行
+interface Product {
+  id: string
+  name: string
+  amount1: string
+  amount2: string
+  amount3: number
+}
+
+interface SummaryMethodProps<T = Product> {
+  columns: TableColumnType[]
+  data: T[]
+}
+
+const sumTableColumns = [
+  { label: 'ID', prop: 'id', width: 180 },
+  { label: 'Name', prop: 'name' },
+  { label: 'Amount 1', prop: 'amount1', sortable: true },
+  { label: 'Amount 2', prop: 'amount2', sortable: true },
+  { label: 'Amount 3', prop: 'amount3', sortable: true }
+] as TableColumnType[]
+
+const getSummaries = (param: SummaryMethodProps) => {
+  const { columns, data } = param
+  const sums: string[] = []
+  columns.forEach((column, index) => {
+    if (index === 0) {
+      sums[index] = 'Total Cost'
+      return
+    }
+    const values = data.map((item) => Number(item[column.property as string]))
+    if (!values.every((value) => Number.isNaN(value))) {
+      sums[index] = `$ ${values.reduce((prev, curr) => {
+        const value = Number(curr)
+        if (!Number.isNaN(value)) {
+          return prev + curr
+        } else {
+          return prev
+        }
+      }, 0)}`
+    } else {
+      sums[index] = 'N/A'
+    }
+  })
+
+  return sums
+}
+const sumTableData: Product[] = [
+  {
+    id: '12987122',
+    name: 'Tom',
+    amount1: '234',
+    amount2: '3.2',
+    amount3: 10,
+  },
+  {
+    id: '12987123',
+    name: 'Tom',
+    amount1: '165',
+    amount2: '4.43',
+    amount3: 12,
+  },
+  {
+    id: '12987124',
+    name: 'Tom',
+    amount1: '324',
+    amount2: '1.9',
+    amount3: 9,
+  },
+  {
+    id: '12987125',
+    name: 'Tom',
+    amount1: '621',
+    amount2: '2.2',
+    amount3: 17,
+  },
+  {
+    id: '12987126',
+    name: 'Tom',
+    amount1: '539',
+    amount2: '4.1',
+    amount3: 15,
+  },
+]
+
+//合并行或列
+interface SpanMethodProps {
+  row: User
+  column: TableColumnType
+  rowIndex: number
+  columnIndex: number
+}
+const arraySpanMethod = ({
+  row,
+  column,
+  rowIndex,
+  columnIndex,
+}: SpanMethodProps) => {
+  if (rowIndex % 2 === 0) {
+    if (columnIndex === 0) {
+      return [1, 2]
+    } else if (columnIndex === 1) {
+      return [0, 0]
+    }
+  }
+}
+
+const objectSpanMethod = ({
+  row,
+  column,
+  rowIndex,
+  columnIndex,
+}: SpanMethodProps) => {
+  if (columnIndex === 0) {
+    if (rowIndex % 2 === 0) {
+      return {
+        rowspan: 2,
+        colspan: 1,
+      }
+    } else {
+      return {
+        rowspan: 0,
+        colspan: 0,
+      }
+    }
+  }
+}
 </script>
 <style scoped></style>

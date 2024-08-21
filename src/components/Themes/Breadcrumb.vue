4134d872db221126e947cc04d51be7a986bb9816
jianghua1 <719584530@qq.com>
2024-08-21 23:35:17 +0800

    4.4.2.3 头部面包屑组件Transition过渡效果两种实现（gs

--- src/components/Themes/Breadcrumb.vue
+++ src/components/Themes/Breadcrumb.vue
@@ -1,17 +1,24 @@
 <template>
   <el-breadcrumb :separator-icon="ArrowRight" class="ml-4">
-    <el-breadcrumb-item :to="{ path: item.name }" v-for="(item, index) in breadcrumnData" :key="item.fullPath">
-      <span>
-        {{ item.meta?.title }}
-      </span>
-    </el-breadcrumb-item>
+
+    <transition-group name="breadcrumb">
+      <!-- <transition-group name="breadcrumb" @enter="onEnter" :css="false"> -->
+      <el-breadcrumb-item :to="{ path: item.name }" v-for="(item, index) in breadcrumnData" :key="item.name"
+        :data-index="index">
+        <span>
+          {{ item.meta?.title }}
+        </span>
+      </el-breadcrumb-item>
+    </transition-group>
   </el-breadcrumb>
 </template>
-x
+
 <script setup lang='tsx'>
 import { useRoute } from 'vue-router/auto'
+import gsap from 'gsap'
 //获取页面路由
 const route = useRoute()
+const change = ref(-1)
 //右箭头
 const ArrowRight = () => <i class="i-ep:arrow-right"></i>
 
@@ -21,8 +28,53 @@ function getBreadcrumnData() {
   breadcrumnData.value = route.matched.filter(item => item.name && item.meta && item.meta.title)
 }
 
+function onEnter(el, done) {
+  let index = el.dataset.index
+  if (change.value === 1) {
+    index = 1
+  }
+  gsap.fromTo(
+    el,
+    {
+      opacity: 0,
+      x: 30
+    }, {
+    opacity: 1,
+    x: 0,
+    delay: index * 0.15,
+    onComplete: done
+  })
+}
+
 watch(route, () => {
   getBreadcrumnData()
-}, { immediate: true })
+},
+  { immediate: true }
+)
+
+watch(breadcrumnData, (newValue, oldValue) => {
+  change.value = 0
+  for (let i = 0; i < Math.max(newValue.length, oldValue.length); i++) {
+    if (newValue.length < i || oldValue.length < i || newValue[i] == oldValue[i]) {
+      change.value++
+    }
+  }
+})
 </script>
-<style scoped></style>
+<style scoped lang="scss">
+.breadcrumb-move,
+.breadcrumb-enter-active,
+.breadcrumb-leave-active {
+  transition: all 0.5s;
+}
+
+.breadcrumb-enter-from,
+.breadcrumb-leave-to {
+  opacity: 0;
+  transform: translateX(20px);
+}
+
+.breadcrumb-leave-active {
+  position: absolute;
+}
+</style>
